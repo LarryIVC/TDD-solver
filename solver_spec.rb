083f8285ba4352886676a12f354e1399aa92cf3a
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-07-14 13:18:13 -0400

    Argument integer greater than 0 green status

--- solver_spec.rb
+++ solver_spec.rb
@@ -2,18 +2,17 @@
 class Solver
   # Create a method called factorial
   def factorial(number)
-
+    raise 'Error the argument is not integer' unless number.is_a?(Integer)
+    raise 'Error the argument is not greater than zero' unless number.positive?
   end
 end
 
 describe Solver do
   describe '#factorial' do
-    # verify that the factorial has one argument
     it 'has one argument' do
       expect(subject).to respond_to(:factorial).with(1).argument
     end
-    # raise an error if verify the argument is integer greater tan zero
-    it 'is not integer greater than zero raise error' do
+    it 'argument is integer greater than zero' do
       solver = Solver.new
       expect { solver.factorial('a') }.to raise_error('Error the argument is not integer')
       expect { solver.factorial(-4) }.to raise_error('Error the argument is not greater than zero')
