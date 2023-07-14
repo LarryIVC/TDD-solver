2398d505adcbface9e08e457052573111495baad
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-07-14 13:20:03 -0400

    Case 1 o 0 return 1 red status

--- solver_spec.rb
+++ solver_spec.rb
@@ -17,5 +17,10 @@ describe Solver do
       expect { solver.factorial('a') }.to raise_error('Error the argument is not integer')
       expect { solver.factorial(-4) }.to raise_error('Error the argument is not greater than zero')
     end
+    it 'return 1 when argument is 1 or 0' do
+      solver = Solver.new
+      expect(solver.factorial(1)).to eq(1)
+      expect(solver.factorial(0)).to eq(1)
+    end
   end
 end
