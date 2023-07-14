cb5fc1947d21ec2e79a3010ac4ee494c0830f782
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-07-14 16:16:19 -0400

    Return reverse string green state

--- solver_spec.rb
+++ solver_spec.rb
@@ -9,6 +9,8 @@ class Solver
 
   def reverse(word)
     raise 'Error the argument is not string' unless word.is_a?(String)
+
+    word.reverse
   end
 end
 
@@ -27,7 +29,7 @@ describe Solver do
       expect(solver.factorial(1)).to eq(1)
       expect(solver.factorial(0)).to eq(1)
     end
-    # calculate factorial
+
     it 'return factorial of argument' do
       solver = Solver.new
       expect(solver.factorial(5)).to eq(120)
