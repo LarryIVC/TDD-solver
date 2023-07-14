4addb2834c0cf421d72235b945e55acf1604c55e
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-07-14 12:59:02 -0400

    Verify to have one argument

--- solver_spec.rb
+++ solver_spec.rb
@@ -4,5 +4,10 @@ class Solver
 end
 
 describe Solver do
-
+  describe '#factorial' do
+    # verify that the factorial has one argument
+    it 'has one argument' do
+      expect(subject).to respond_to(:factorial).with(1).argument
+    end
+end
 end 
